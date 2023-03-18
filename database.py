44530046dc5719790b12db48f1705f4bb927ac03
Jupiee <97674299+Jupiee@users.noreply.github.com>
2023-03-18 15:44:30 +0500

    Delete database.py

--- database.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client= MongoClient("mongodb+srv://Jupie:tigerness@cluster0.np31lmg.mongodb.net/?retryWrites=true&w=majority")
-
-collection= client["cached_comics"]["comics"]
