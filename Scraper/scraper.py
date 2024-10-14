57bf5728f05fa6fb629191e6996da44a51a6fad0
Jupiee <mzkplayz1@gmail.com>
2024-10-14 20:19:39 +0500

    feat: implement fetching of article nodes with improved performance

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,5 +1,5 @@
-import asyncio, aiohttp, re
-from selectolax.parser import HTMLParser
+import asyncio, re, httpx
+from selectolax.lexbor import LexborHTMLParser
 from datetime import datetime, timedelta
 from database import collection
 
@@ -9,8 +9,38 @@ class Scraper:
 
         self.base_url= "https://getcomics.org"
         self.url= None
-        self.query= None
-        self.page= 0
         #self.total_pages= 0
         self.articles= None
-        self.session= None
+        self.session= httpx.AsyncClient()
+
+    async def get_search_results(self, query, page):
+
+        self.url= f"{self.base_url}/page/{page}/?s={query}"
+
+        response= await self.session.get(self.url)
+
+        is_valid_page= response.status_code != 404
+
+        if not is_valid_page:
+        
+            return 202
+        
+        return await self.extract_articles()
+    
+    async def extract_articles(self):
+
+        data= {"Meta-Data": []}
+
+        response= await self.session.get(self.url)
+
+        parser= LexborHTMLParser(response.text)
+
+        articles= parser.css_first("div.post-list-posts")
+
+        if not articles:
+
+            return 201
+        
+        articles= articles.css("article")
+
+        return articles
