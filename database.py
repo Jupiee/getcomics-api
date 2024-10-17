f16ac27cff8352f81b50d5e00b28ded4aadf444f
Jupiee <mzkplayz1@gmail.com>
2024-10-17 18:03:18 +0500

    feat: add cache manager

--- database.py
+++ database.py
@@ -1,9 +1,47 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import os
+from datetime import timedelta, datetime
+import os, pytz
 
 load_dotenv()
 
 client= MongoClient(os.getenv("URI"))
 
-collection= client["cached_comics"]["comics"]
+class CacheManager:
+
+    def __init__(self):
+
+        self.collection= client["cached_comics"]["comics"]
+        self.cache_interval= timedelta(hours= 24)
+        self.time_zone= pytz.UTC
+
+    def create_cache(self, data):
+
+        cache_time= datetime.now(tz= self.time_zone)
+
+        data["cache_time"]= cache_time
+
+        self.collection.insert_one(data)
+
+    def update_cache(self, filter, updated_data):
+
+        cache_time= datetime.now(tz= self.time_zone)
+
+        updated_data["$set"]["cache_time"]= cache_time
+
+        self.collection.update_one(filter, updated_data)
+
+    def fetch_cache(self, filter):
+
+        return self.collection.find_one(filter)
+
+    def is_cache_expired(self, cache_time):
+
+        cache_time_aware= self.time_zone.localize(cache_time)
+        now= datetime.now(self.time_zone)
+
+        if now - cache_time_aware < self.cache_interval:
+
+            return False
+        
+        return True
--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from fastapi.routing import APIRouter
 from Scraper import Scraper
 from StatusCodes import codes
 from Schemas import MetaData
+from database import CacheManager
 
 VERSION= "2.1.0"
 
 scraper= Scraper()
+cache_manager= CacheManager()
 app= FastAPI(title= "getcomics API", version= VERSION)
 
 api_router= APIRouter()
@@ -20,6 +22,18 @@ async def root():
 @api_router.get("/search/{query}")
 async def search(query: str = None, page: int = 1) -> MetaData:
 
+    filter= {"Query": query, "Page": page}
+
+    cache= cache_manager.fetch_cache(filter)
+
+    if cache != None:
+
+        cache_expired= cache_manager.is_cache_expired(cache["cache_time"])
+
+        if not cache_expired:
+
+            return MetaData(Meta_Data= cache["Comics"]["Meta-Data"])
+
     scraper.set_search_url(query, page)
 
     results= await scraper.get_search_results()
@@ -27,20 +41,60 @@ async def search(query: str = None, page: int = 1) -> MetaData:
     if isinstance(results, int):
 
         raise HTTPException(status_code= results, detail= codes[results])
+    
+    if cache == None:
+    
+        cache_manager.create_cache({"cache_time": None, "Query": query, "Page": page, "Comics": results})
+
+    else:
+
+        cache_manager.update_cache(filter, {"$set":{"cache_time": None, "Comics": results}})
 
     return MetaData(Meta_Data= results["Meta-Data"])
 
 @api_router.get("/latest")
 async def latest() -> MetaData:
 
+    filter= {"Latest": True}
+    
+    cache= cache_manager.fetch_cache(filter)
+
+    if cache != None:
+
+        cache_expired= cache_manager.is_cache_expired(cache["cache_time"])
+
+        if not cache_expired:
+
+            return MetaData(Meta_Data= cache["Comics"]["Meta-Data"])
+
     scraper.set_latestpage_url()
 
     result= await scraper.get_search_results()
+
+    if  cache == None:
+
+        cache_manager.create_cache({"cache_time": None, "Latest": True, "Comics": result})
+
+    else:
+
+        cache_manager.update_cache(filter, {"$set":{"cache_time": None, "Comics": result}})
 
     return MetaData(Meta_Data= result["Meta-Data"])
 
 @api_router.get("/tag/{tag}")
 async def tag(tag: str = None, page: int = 1) -> MetaData:
+
+    filter= {"Tag": tag, "Page": page}
+
+    cache= cache_manager.fetch_cache(filter)
+
+    if cache != None:
+
+        cache_expired= cache_manager.is_cache_expired(cache["cache_time"])
+
+        if not cache_expired:
+
+            return MetaData(Meta_Data= cache["Comics"]["Meta-Data"])
 
     scraper.set_tag_url(tag, page)
 
@@ -49,6 +103,14 @@ async def tag(tag: str = None, page: int = 1) -> MetaData:
     if isinstance(results, int):
 
         raise HTTPException(status_code= results, detail= codes[results])
+    
+    if cache == None:
+    
+        cache_manager.create_cache({"cache_time": None, "Tag": tag, "Page": page, "Comics": results})
+
+    else:
+
+        cache_manager.update_cache(filter, {"$set":{"cache_time": None, "Comics": results}})
 
     return MetaData(Meta_Data= results["Meta-Data"])
 
